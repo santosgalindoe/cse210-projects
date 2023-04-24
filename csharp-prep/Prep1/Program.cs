11cbe3480167dc020d4cba5c238ac58918edb96e
Luis E. Santos Galindo <santosgalindoe@outlook.com>
2023-04-24 11:35:41 -0600

    prep1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,11 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("What is your first name?" );
+        Console.Write("What is your first name?" );
         string firstname = Console.ReadLine();
-        Console.WriteLine("What is your last name?" );
+
+        Console.Write("What is your last name?" );
         string lastname = Console.ReadLine();
-        string Firstname_Lastname = ($"Your name is {firstname}, {lastname}.");
+
+        Console.WriteLine($"Your name is {lastname}, {firstname} {lastname}.");
     }
 
 }
