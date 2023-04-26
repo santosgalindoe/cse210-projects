83a5f5a8297606d495da0c93b476fb0d4a45e0d3
Luis Galindo <santosgalindoe@byui.edu>
2023-04-26 12:02:47 -0600

    changecd

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random ();
+        int number = randomGenerator.Next (1,11); 
+
+        Console.Write($"What is the magic number? {number} "); 
+        string mnumber = Console.ReadLine();
+        
+        Console.Write("What is your guess? ");
+        string gnumber = Console.ReadLine(); 
+
+        while (mnumber == gnumber)
+        {
+            Console.Write("You guessed it");
+        }
+
+
     }
 }
